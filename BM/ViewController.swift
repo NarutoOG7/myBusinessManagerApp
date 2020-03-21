27eef84dd7e85baf4cf0e4eda3196d0503eaafce
Spencer Belton <23slowmojo@gmail.com>
2020-03-20 21:01:56 -0600

    Initial Commit

---
+++ BM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BM
+//
+//  Created by Spencer Belton on 3/20/20.
+//  Copyright © 2020 Spencer Belton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
